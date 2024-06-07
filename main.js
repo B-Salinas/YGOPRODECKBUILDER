c6c07bd48286c1b3871f2c9684be1eda40719c7c
B Salinas <b.salinas397@gmail.com>
2024-06-06 19:46:56 -0500

    saving my progress for the day

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const axios = require("axios");
-
 const api_url = "https://db.ygoprodeck.com/api/v7/cardinfo.php";
 
 axios
   .get(api_url)
-  .then((res) => {
-    const cards = res.data;
-    console.log(cards);
+  .then((api_res) => {
+    const raw = api_res.data;
+    const cards = raw.data;
+
   })
   .catch((err) => {
     console.log(`Uh oh! Something went wrong: ${err.res.status}`);
