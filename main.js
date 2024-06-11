049881cce06843ac1a1e3b7ff3de5d163e0d8330
B Salinas <b.salinas397@gmail.com>
2024-06-11 08:00:15 -0500

    last time, was playing around w ChatGPT's suggestions. Erased them for now to start from scratch

--- main.js
+++ main.js
@@ -5,9 +5,12 @@ axios
   .get(api_url)
   .then((api_res) => {
     const raw = api_res.data;
-    const cards = raw.data;
+    const cards = raw.data; // key into main object
+
+    // console.log(Object.keys(cards[0])); // returns an array of strings, using keys from the first card
+    // console.log(Object.keys(cards[0]).length); // 1
 
   })
-  .catch((err) => {
+  .catch((api_err) => {
     console.log(`Uh oh! Something went wrong: ${err.res.status}`);
   });
